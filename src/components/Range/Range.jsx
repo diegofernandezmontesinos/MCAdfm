4b313ba8bf7313978ecd4b75c068f246239a3585
diego <dfmontesinos@gmail.com>
2024-04-09 06:17:14 +0200

    start range slider

---
+++ src/components/Range/Range.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Range() {
+  return (
+    <>
+      <div className="app">
+
+      <div className="box">
+        <h3>Price Range</h3>
+        <div>
+          <span className="value">$100</span> -   <span className="value">$15000</span>
+        </div>
+        <small>Current Range: $2325</small>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default Range;
