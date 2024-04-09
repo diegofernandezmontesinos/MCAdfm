cb82143cbb526cd435b39b2489b83f700a769990
diego <dfmontesinos@gmail.com>
2024-04-09 16:57:58 +0200

    other range slider

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,41 @@ import Landing from "./components/LandingPage/landing";
 import { Parameter } from "./share/typeExercise";
 
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const _handleIndexChange = (index) => {
+    setCurrentIndex(index);
+  };
+
+  const _handleNext = (currentIndex) => {
+    setCurrentIndex(currentIndex + 1);
+  };
+
+  const _handleComplete = () => {};
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route
           path="/sliderExercise1"
-          element={<Range variant={Parameter.normal} />}
+          element={
+            <Range
+              onChange={_handleIndexChange}
+              currentIndex={currentIndex}
+              variant={Parameter.normal}
+            />
+          }
         />
         <Route
           path="/sliderExercise2"
-          element={<Range variant={Parameter.fixed} />}
+          element={
+            <Range
+              onChange={_handleIndexChange}
+              currentIndex={currentIndex}
+              variant={Parameter.fixed}
+            />
+          }
         />
       </Routes>
     </BrowserRouter>
--- src/components/Range/Range.jsx
+++ src/components/Range/Range.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import "./Range.css";
 import Slider from "react-slider";
+import { Parameter } from "../../share/typeExercise";
+import ReactSlider from "react-slider";
 
-function Range(variant) {
+function Range(props) {
   const MIN = 0;
   const MAX = 15000;
   const [values, setValues] = useState([MIN, MAX]);
@@ -13,19 +15,46 @@ function Range(variant) {
       <div className="app">
         <div className="box">
           <h3>Price Range</h3>
-          <div className={"value"}>
-            ${values[0]} - ${values[1]}
-          </div>
-          <small>Current Range: ${values[1] - values[0]}</small>
-          <Slider
-            className={"slider"}
-            onChange={setValues}
-            value={values}
-            thumbClassName={thumbGrabbed ? "thumb" : "thumbGrabbed"}
-            min={MIN}
-            max={MAX}
-            onClick={() => setThumbGrabbed(!thumbGrabbed)}
-          />
+          {props.variant == Parameter.normal ? (
+            <>
+              <div className={"value"}>
+                ${values[0]} - ${values[1]}
+              </div>
+              <small>Current Range: ${values[1] - values[0]}</small>
+              <Slider
+                className={"slider"}
+                onChange={setValues}
+                value={values}
+                thumbClassName={thumbGrabbed ? "thumb" : "thumbGrabbed"}
+                min={MIN}
+                max={MAX}
+                onClick={() => setThumbGrabbed(!thumbGrabbed)}
+              />
+            </>
+          ) : (
+            <>
+              <ReactSlider
+                className="vertical-slider"
+                markClassName="example-mark"
+                // onChange={onChange}
+                trackClassName="example-track"
+                defaultValue={0}
+                value={props.currentIndex}
+                min={0}
+                max={4}
+                marks
+                renderMark={(props) => {
+                  if (props.key < props.currentIndex) {
+                    props.className = "example-mark example-mark-completed";
+                  } else if (props.key === props.currentIndex) {
+                    props.className = "example-mark example-mark-active";
+                  }
+                  return <span {...props} />;
+                }}
+                orientation="horizontal"
+              />
+            </>
+          )}
         </div>
       </div>
     </>
