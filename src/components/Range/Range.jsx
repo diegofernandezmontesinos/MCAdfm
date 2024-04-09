ba193db1e93ef6c204637a242f39781d914e1592
diego <dfmontesinos@gmail.com>
2024-04-09 12:23:57 +0200

    more code to the component Range and his style

--- src/components/Range/Range.jsx
+++ src/components/Range/Range.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import './Range.css';
+import "./Range.css";
 
 function Range() {
   return (
     <>
       <div className="app">
-
-      <div className="box">
-        <h3>Price Range</h3>
-        <div>
-          <span className="value">$100</span> -   <span className="value">$15000</span>
+        <div className="box">
+          <h3>Price Range</h3>
+          <div className={"value"}>$100 - $15000</div>
+          <small>Current Range: $2325</small>
         </div>
-        <small>Current Range: $2325</small>
-      </div>
       </div>
     </>
   );
